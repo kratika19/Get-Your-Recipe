73d587dfd9ee5145483af23b1434569b8784fce8
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-05-28 15:52:49 +0530

    getting api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import React, { useState } from 'react'
 
 function App() {
+
+  const [query, setquery] = useState("");
+  const appId = "7bc88129";
+  const apiKey = "e1fdaad97887812a20874ed8bb1e0297";
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${apiKey}&health=alcohol-free`;
+
+  const getRecipe = async () => {
+    var result = await Axios.get(url);
+    console.log(result.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onClick={getRecipe}>🥗 GET YOUR RECIPE 🥗</h1>
+      <form action="" className="app-search-form">
+        <input placeholder="Enter ingredient" type="text" value={query} onChange={(e) => { setquery(e.target.value) }} />
+
+      </form>
     </div>
   );
 }
