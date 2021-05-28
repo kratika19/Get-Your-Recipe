0ce7e8a84dd1bd98e47d3fd6fc67eec4fb7d5555
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-05-28 20:05:50 +0530

    removed warnings

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -6,8 +6,8 @@ export default function RecipeTile({ recipe }) {
     return (
         recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
             <div className="recipe-tile">
-                <a href={recipe["recipe"]["url"]} target="_blank">
-                <img className="recipe-img" src={recipe["recipe"]["image"]} /></a>
+                <a href={recipe["recipe"]["url"]} target="_blank" rel="noreferrer">
+                    <img className="recipe-img" src={recipe["recipe"]["image"]} alt={recipe["recipe"]["label"]} /></a>
                 <p className="recipe-name">{recipe["recipe"]["label"]}</p>
             </div>
         )
